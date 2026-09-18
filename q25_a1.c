7067bb466a2fe174c61c422d24839434b5857357
Vanshit Shah <vanshit.s@ahduni.edu.in>
2026-09-18 19:30:46 +0530

    latest change

--- q25_a1.c
+++ q25_a1.c
@@ -6,3 +6,10 @@ int main()
 
     return 0;
 }
+
+
+//  gcc q25_a1.c -o hello
+// vanshit@Vanshit:~/college$ gcc q25_a2.c
+// vanshit@Vanshit:~/college$ ./a.out
+// Executing hello program...
+// Hello from the executed program.
--- q25_a2.c
+++ q25_a2.c
@@ -13,3 +13,10 @@ int main()
 
     return 1;
 }
+
+
+//  gcc q25_a1.c -o hello
+// vanshit@Vanshit:~/college$ gcc q25_a2.c
+// vanshit@Vanshit:~/college$ ./a.out
+// Executing hello program...
+// Hello from the executed program.
